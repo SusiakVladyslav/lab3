c0d9a9ac6163940a6df47138b2b1d0f43cc29b31
Vladyslav Susiak <vladyslavsusiak@gmail.com>
2024-12-20 20:59:51 +0200

    United 2 switches to the one that is easier to read

--- src/main/java/org/example/GlidedRose.java
+++ src/main/java/org/example/GlidedRose.java
@@ -15,6 +15,9 @@ class GildedRose {
                     if (item.quality < 50) {
                         item.increaseQualityByOne();
                     }
+                    if (item.sellIn < 0) {
+                        item.decreaseQualityByOne();
+                    }
                     break;
                 case "Backstage passes to a TAFKAL80ETC concert":
                     item.increaseQualityByOne();
@@ -25,34 +28,20 @@ class GildedRose {
                     if (item.sellIn < 6) {
                         item.increaseQualityByOne();
                     }
+                    if (item.sellIn < 0) {
+                        item.quality = 0;
+                    }
                     break;
                 case "Sulfuras, Hand of Ragnaros":
                     item.sellIn -= 1;
                     break;
                 default:
                     item.decreaseQualityByOne();
-                    break;
-
-            }
-
-            switch(item.name) {
-                case "Aged Brie":
-                    if (item.sellIn < 0) {
-                        item.decreaseQualityByOne();
-                    }
-                    break;
-                case "Backstage passes to a TAFKAL80ETC concert":
-                    if (item.sellIn < 0) {
-                        item.quality = 0;
-                    }
-                    break;
-                case "Sulfuras, Hand of Ragnaros":
-                    break;
-                default:
                     if (item.quality > 0) {
                         item.decreaseQualityByOne();
                     }
                     break;
+
             }
         }
     }
