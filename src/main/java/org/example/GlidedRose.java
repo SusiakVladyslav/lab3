ed8b1a5453331e2cf343e59d2dd837aacee5bce2
Vladyslav Susiak <vladyslavsusiak@gmail.com>
2024-12-20 20:04:44 +0200

    Changed hard to read if sequence to easy-to-read switch

--- src/main/java/org/example/GlidedRose.java
+++ src/main/java/org/example/GlidedRose.java
@@ -32,11 +32,34 @@ class GildedRose {
                 item.sellIn = item.sellIn - 1;
             }
 
+            switch(item.name) {
+                case "Aged Brie":
+                    if (item.sellIn < 0) {
+                        item.decreaseQualityByOne();
+                    }
+                    break;
+                case "Backstage passes to a TAFKAL80ETC concert":
+                    if (item.sellIn < 0) {
+                        item.quality = 0;
+                    }
+                    break;
+                case "Sulfuras, Hand of Ragnaros":
+                    break;
+                default:
+                    if (item.quality > 0) {
+                        item.decreaseQualityByOne();
+                    }
+            }
+
+
+
             if (item.sellIn < 0) {
                 if (!item.name.equals("Aged Brie")) {
                     if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                            item.decreaseQualityByOne();
+                        if (item.quality > 0) {
+                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                                item.decreaseQualityByOne();
+                            }
                         }
                     } else {
                         item.quality = 0;
